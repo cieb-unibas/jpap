2bb8fcbe2347e8a9a72aa362d76e4d4d8a6d42ea
matthnig <matthias.niggli@unibas.ch>
2023-03-03 11:27:28 +0100

    adapted test function for utils

--- jpap/tests/test_jpap_utils.py
+++ jpap/tests/test_jpap_utils.py
@@ -3,8 +3,6 @@ import random
 
 import jpap
 
-# fixtures
-@pytest.fixture
 def simulate_string():
     string_length = random.randint(1, 10)
     random_string = "".join([chr(c) for c in random.choices(population=range(97,122), k = string_length)])
@@ -17,7 +15,7 @@ def test_spf_input():
         with pytest.raises(TypeError):
             jpap.sql_pattern_formatting(patterns = i)
 
-def test_spf_output(simulate_string):
+def test_spf_output():
     random.seed(1)
     input_patterns = [simulate_string() for x in range(10)]
     formatted_strings = jpap.sql_pattern_formatting(input_patterns)
