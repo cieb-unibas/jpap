118a1670567c1226aa4e2d1de2d1bcd4c4cb691b
matthnig <matthias.niggli@unibas.ch>
2023-04-27 12:20:50 +0200

    ipl training pipeline incorporated. gpu memory problems detected

--- scripts/ipl/ipl_train.py
+++ scripts/ipl/ipl_train.py
@@ -3,12 +3,15 @@ import os
 import sys
 
 import torch
+from torch.utils.data import Dataset, DataLoader
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
 import pandas as pd
 
-from jpap.preprocessing import encode_labels
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, f1_score, classification_report
 
-#### paths / directories
 def set_paths():
     try:
         HOME = os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, os.pardir))
@@ -17,7 +20,6 @@ def set_paths():
     sys.path.append(HOME)
     return HOME
 
-#### extract company descriptions
 def load_labelled(n_postings : int = None, home_dir: str = set_paths()) -> pd.DataFrame:
     """
     Load n postings' employer descriptions that were manually labelled to industries.
@@ -30,60 +32,189 @@ def load_labelled(n_postings : int = None, home_dir: str = set_paths()) -> pd.Da
         df = df.reset_index(drop=True)
     return df
 
-#### extract company descriptions
-def load_xlm_pretrained(path_to_model = None):
+def load_xlm_pretrained(path_to_model = None, n_targets: int = 16):
     if path_to_model:
         tokenizer = AutoTokenizer.from_pretrained(path_to_model)
-        model = AutoModelForSequenceClassification.from_pretrained(path_to_model)
+        model = AutoModelForSequenceClassification.from_pretrained(path_to_model, num_labels = n_targets)
     else:
         tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-base")
-        model = AutoModelForSequenceClassification.from_pretrained("xlm-roberta-base")
+        model = AutoModelForSequenceClassification.from_pretrained("xlm-roberta-base", num_labels = n_targets)
     return tokenizer, model
 
+def split_data(x, y, validation_set = True, test_size: float = 0.15, val_size: float = 0.25, random_state = None):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= test_size, random_state=random_state)
+    if validation_set:
+        x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=val_size, random_state=random_state)
+        return x_train, x_val, x_test, y_train, y_val, y_test
+    else:
+        return x_train, x_test, y_train, y_test
+
+def get_label_encoder(y):
+    le = LabelEncoder()
+    le.fit(y)
+    return le
+
+def encode_labels(label_encoder, y, torch_dtype = torch.int32):
+    labels = torch.tensor(label_encoder.transform(y), dtype=torch_dtype)
+    return labels
+
+def text_tokenizer(text_partitions: list, return_tensors="pt", 
+    truncation=True, max_length=128, padding=True):
+    for i, t in enumerate(text_partitions):
+        text_partitions[i] = tokenizer(t.to_list(), return_tensors=return_tensors,
+                                       truncation=truncation, max_length=max_length,
+                                       padding = padding)
+    
+    return text_partitions[0], text_partitions[1], text_partitions[2] 
+
+
+class IndustryClassificationDataset(Dataset):
+    def __init__(self, targets, texts) -> None:
+        super().__init__()
+        self.targets = targets
+        self.texts = texts
+
+    def __len__(self):
+        return len(self.targets)
+    
+    def __getitem__(self, idx):
+        item = {key: value[idx] for key, value in self.texts.items()}
+        item["labels"] = self.targets[idx]
+        return item
+    
+
+    
 #### load xlm-roberta-base-classifier:
-tokenizer, model = load_xlm_pretrained()
+tokenizer, model = load_xlm_pretrained(n_targets= 16)
 
 #### load and split training datat
 df = load_labelled()
-df.groupby(["industry"])["industry"].count().sort_values(ascending=False)
+x_train, x_val, x_test, y_train, y_val, y_test = split_data(x = df["employer_description"], y = df["industry"], 
+                                                            validation_set = True,  test_size=0.15, val_size=0.25,
+                                                            random_state = 27042023)
 
-# !!! => do this using sklearn... !!!
+#### tokenization:
+le = get_label_encoder(y = df["industry"])
+x_train, x_val, x_test = text_tokenizer(text_partitions = [x_train, x_val, x_test])
+y_train, y_val, y_test = encode_labels(label_encoder=le, y = y_train)\
+    , encode_labels(label_encoder=le, y = y_val)\
+    , encode_labels(label_encoder=le, y = y_test) 
 
-#### tokenize the input and label the targets
-# targets
-n = 10
-labels, label_dict = encode_labels(inputs = df["industry"], return_label_dict = True)
-labels = torch.tensor(labels[:n], dtype=torch.int32)
-
-# features
-inputs = tokenizer(
-    df["employer_description"][:n].to_list(), return_tensors="pt", 
-    truncation=True, max_length=128, padding=True
+# pipeline
+BATCH_SIZE = 8
+train_dl = DataLoader(
+    IndustryClassificationDataset(targets = y_train, texts = x_train),
+    batch_size = BATCH_SIZE, shuffle = True
     )
 
-#### train/fine-tune xlm-roberta-base for industry classification
+val_dl = DataLoader(
+    IndustryClassificationDataset(targets = y_val, texts = x_val),
+    batch_size = BATCH_SIZE, shuffle = False
+    )
 
-#### evaluate the fine-tuned classifier:
+optim = torch.optim.AdamW(model.parameters(), lr=5e-5)
+batch = next(iter(train_dl))
+train_device = "cuda"
+torch.backends.cudnn.deterministic = True
 
-#### save the fine-tuned classifier
+# torch.cuda.empty_cache()
+# os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:64"
+
+def finetune(model, n_epochs : int, train_loader, train_device: str = "cuda"):
+    
+    model.to(train_device)
+    
+    for epoch in range(n_epochs):
+        
+        model.train()
+        correct_samples, n_samples = 0, 0
+
+        for batch in train_loader:
+            
+            # define inputs and labels:
+            x = batch["input_ids"].to(train_device)
+            mask = batch["attention_mask"].to(train_device)
+            y = batch["labels"].type(torch.LongTensor).to(train_device)
+            
+            # forward pass:
+            outputs = model(x, attention_mask = mask, labels = y)
+            loss = outputs["loss"]
+            n_samples += y.size(0)
+            correct_samples += (torch.argmax(outputs["logits"], dim = 1) == y).sum().item()
+
+            # clean up gpu memory
+            del x
+            del y
+            del mask
+            del outputs
+            torch.cuda.empty_cache()
+
+            # loss and update weights
+            optim.zero_grad()
+            loss.backward()
+            optim.step()
+            del loss
+
+        print("Epoch %d/%d | Training Accuracy: %d" % (epoch, n_epochs, float(correct_samples) / n_samples))
+
+    print("Training finished. Returning fine-tuned model")
+    return model
+        
+
+def evaluate_finetuned(model, eval_loader, train_device : str = "cuda"):
+    """
+    Evaluate on testing set
+    """
+               
+# memory run-out
+# https://www.kaggle.com/getting-started/140636
+# https://stackoverflow.com/questions/63145729/how-to-make-sure-pytorch-has-deallocated-gpu-memory
+
 
 
 if __name__ == "__main__":
-    # load data
-    df = load_labelled()
-
-    # split data
-
-    # load classifier
     
-    # tokenize data
+    # load data & split data
+    df = load_labelled()
+    x_train, x_val, x_test, y_train, y_val, y_test = split_data(x = df["employer_description"], y = df["industry"], 
+                                                            validation_set = True,  test_size=0.15, val_size=0.25,
+                                                            random_state = 27042023)
+    print("Number of training samples: %d" %len(x_train))
+    print("Number of validation samples: %d" %len(x_val))
+    print("Number of testing samples: %d" %len(x_test))
 
-    # fine-tune
-    # (a) train only
-    # (b) train & validation
+    # load encoder, tokenizer and model
+    le = get_label_encoder(y = df["industry"])
+    tokenizer, model = load_xlm_pretrained(n_targets= len(le.classes_))
+    print("Label encoder, pre-trained tokenizer and pre-trained model checkpoint loaded.")
+
+    # tokenize the data
+    le = get_label_encoder(y = df["industry"])
+    x_train, x_val, x_test = text_tokenizer(text_partitions = [x_train, x_val, x_test])
+    y_train, y_val, y_test = encode_labels(label_encoder=le, y = y_train)\
+        , encode_labels(label_encoder=le, y = y_val)\
+        , encode_labels(label_encoder=le, y = y_test) 
+
+    # data pipeline
+    BATCH_SIZE = 16
+    train_dl = DataLoader(
+        IndustryClassificationDataset(targets = y_train, texts = x_train),
+        batch_size = BATCH_SIZE, shuffle = True
+        )
+
+    # configurations for finetuning the model
+    DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    EPOCHS = 2
+    model.to(DEVICE)
+    model.train()
+    optim = torch.optim.Adam(model.parameters(), lr=5e-5)
+    finetuned_model = finetune(model = model, n_epochs = 2, train_loader = train_dl, train_device= "cuda")
 
     # evaluate
-    # (a) on eval
-    # (b) on test
+    val_dl = DataLoader(
+        IndustryClassificationDataset(targets = y_val, texts = x_val),
+        batch_size = BATCH_SIZE, shuffle = False
+        )
+    evaluate_finetuned(model=finetuned_model, eval_loader = val_dl, train_device= "cuda")
 
-    # if final: save
+    # if final: retrain on everything and save
